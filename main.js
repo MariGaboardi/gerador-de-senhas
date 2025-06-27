3a35d7f111051a57808e29648e1f97d45979c10c
MariGaboardi <160857358+MariGaboardi@users.noreply.github.com>
2025-06-27 08:22:55 -0300

    sdffffffff

--- main.js
+++ main.js
@@ -75,7 +75,8 @@ forcaSenha.classList.add('forte');
 }
 else if (tamanhoSenha > 5 && tamanhoSenha < 12 ){
 forcaSenha.classList.add('media');
-}
+} else if (tamanhoSenha <= 5){
 
 forcaSenha.classList.add('forte');
+}
 }
